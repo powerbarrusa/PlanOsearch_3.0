a5ee47b9e9d8e179b6bc95da5b09fa440c25d849
powerbarrusa <barr@covrigaru.com>
2019-03-31 14:56:04 -0600

    embedded a single, static iframe

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,10 @@ const Search = (props) => {
 
   return (
     <div className="container">
-      Search Page
+      <h1>
+        Search
+      </h1>
+      <iframe title="iframe" src="https://planotours.planomatic.com/tour/187136/tourmini" height="515" width="663" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
     </div>
   )
 }
