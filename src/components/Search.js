56c51c44ecb0f78a932262c0f321e36f89b567e2
powerbarrusa <barr@covrigaru.com>
2019-04-01 11:15:14 -0600

    bootstrap cards showing

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,6 +10,22 @@ const Search = (props) => {
       {props.listings.map((listing, idx) => {
         return (
           <div key={idx}>
+            <div className="card listing">
+              <img className="card-img-top" src="..." alt="Card cap" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Cras justo odio</li>
+                <li className="list-group-item">Dapibus ac facilisis in</li>
+                <li className="list-group-item">Vestibulum at eros</li>
+              </ul>
+              <div className="card-body">
+                <a href="/" className="card-link">Card link</a>
+                <a href="/" className="card-link">Another link</a>
+              </div>
+            </div>
             {listing.state}
           </div>
           )
