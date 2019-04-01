341b8974ce70f0874208fabcad3e1d2207276afc
powerbarrusa <barr@covrigaru.com>
2019-04-01 11:18:41 -0600

    listing data flowing to each card in search

--- src/components/Search.js
+++ src/components/Search.js
@@ -17,16 +17,15 @@ const Search = (props) => {
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
               <ul className="list-group list-group-flush">
-                <li className="list-group-item">Cras justo odio</li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Vestibulum at eros</li>
+                <li className="list-group-item">{listing.street}</li>
+                <li className="list-group-item">{listing.city}</li>
+                <li className="list-group-item">{listing.state}</li>
               </ul>
               <div className="card-body">
                 <a href="/" className="card-link">Card link</a>
                 <a href="/" className="card-link">Another link</a>
               </div>
             </div>
-            {listing.state}
           </div>
           )
         })
