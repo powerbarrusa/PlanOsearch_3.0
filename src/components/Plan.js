9df2b1c68f17e28a49d03f8adba795fceedd0dca
powerbarrusa <barr@covrigaru.com>
2019-04-10 17:27:03 -0600

    good iframe sizing

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -10,7 +10,7 @@ const Plans = (props) => {
       <div className="plan">
         <Card>
           <Card.Body>
-            <iframe title="iframe" floorwidth='840' src='https://planotours.planomatic.com/tour/178885/tourmini' thumbnail='https://photoplan-cache.cdn.planomatic.com/tourFiles/178885/photocache/5423490-300wh.jpg' height="600" width="800" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
+            <iframe title="iframe" floorwidth='880' src='https://planotours.planomatic.com/tour/178885/tourmini' thumbnail='https://photoplan-cache.cdn.planomatic.com/tourFiles/178885/photocache/5423490-300wh.jpg' height="650" width="900" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
           </Card.Body>
         </Card>
       </div>
